4e19063bb0d4c0b1f781324802a1135ad2224ef9
sumitwalke <sumitwalke123@gmail.com>
2023-12-30 19:52:34 +0530

    Setting up Firebase Config file

---
+++ netflix/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC79skfLWBt3eZ2kF9veZ9LjChgsLtb9qc",
+    authDomain: "netflix-clone-react-130f8.firebaseapp.com",
+    projectId: "netflix-clone-react-130f8",
+    storageBucket: "netflix-clone-react-130f8.appspot.com",
+    messagingSenderId: "114569505062",
+    appId: "1:114569505062:web:3c5f8027a60fcb2480b9e7"
+  };
